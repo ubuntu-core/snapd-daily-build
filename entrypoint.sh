d04bd8b71610d3fbabc0d9e454f0041260f7843e
Federico Gimenez <fgimenez@coit.es>
2017-01-17 11:56:57 +0100

    fix deb signing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
-set -ex
+#!/bin/bash
+#set -e
 
-git clone https://github.com/snapcore/snapd ${SNAPCORE_PATH}
+git clone https://github.com/snapcore/snapd ${SNAPD_PATH}
 
 cd ${SNAPD_PATH}
 GIT_COMMIT=$(git log --pretty=format:"%H" | head -1)
@@ -14,14 +14,15 @@ $GOPATH/bin/govendor sync
 
 rm -rf ../*.deb ../*.changes ../*.dsc ../*.tar.xz ../*.upload
 
-dch --distribution xenial -m --local +ppa$TRAVIS_BUILD_NUMBER.$(echo $GIT_COMMIT|cut -b1-8)- Daily release to the PPA
+dch --distribution xenial -m --local +ppa${TRAVIS_BUILD_NUMBER}.$(echo $GIT_COMMIT|cut -b1-8)- Daily release to the PPA
 
-echo $BUILD_PASSWORD > ./build_password
-echo $BUILD_KEY > ./build_key
-gpg ---import ./build_key
-echo "gpg --sign --batch --passphrase-file ./build_password \$@" > sign.sh
+echo -n "$BUILD_PASSWORD" > ${PWD}/build_password
+echo -n "$BUILD_KEY" > ${PWD}/build_key
+gpg-agent --daemon --enable-ssh-support
+gpg --import ${PWD}/build_key
+echo "gpg --sign --batch --passphrase-file ${PWD}/build_password \$@" > ${PWD}/sign.sh
 chmod +x sign.sh
-dpkg-buildpackage -S --force-sign -kDB810A52 -p./sign.sh
+dpkg-buildpackage -S --force-sign -kDB810A52 -p${PWD}/sign.sh
 lintian
 
 # dput ppa:snappy-dev/edge ../*.changes
